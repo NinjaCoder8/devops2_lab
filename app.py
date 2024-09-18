1c8c30bf01ec82851cc030c42f80d88be43e6fc1
Charbel <charbel.dawoud@gmail.com>
2024-09-18 18:47:00 +0200

    breaking pylint

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-"""
-This code is a test for devops github actions
-"""
+import math
 
 def add_numbers(x, y):
-    """
-    This is a test function
-    """
     z = x + y
     return z
 
 def test_function():
-    """
-    This is a test function
-    """
+    pass
+
